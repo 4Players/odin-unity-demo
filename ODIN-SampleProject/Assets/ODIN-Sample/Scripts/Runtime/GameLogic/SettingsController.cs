6384b66f050893450392990932808815b73c6b12
David Liebemann <david@crystalmesh.de>
2022-03-11 18:49:31 +0100

    Added settings menu - Added a settings menu - Can now toggle between voice activation and push to talk for the 3d voice chat

---
+++ ODIN-SampleProject/Assets/ODIN-Sample/Scripts/Runtime/GameLogic/SettingsController.cs
@@ -0,0 +1,29 @@
+﻿using ODIN_Sample.Scripts.Runtime.Odin;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace ODIN_Sample.Scripts.Runtime.GameLogic
+{
+    public class SettingsController : MonoBehaviour
+    {
+        [SerializeField] private OdinStringVariable toggleSettingsButton;
+        [SerializeField] private GameObject settingsRoot;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(toggleSettingsButton);
+            Assert.IsNotNull(settingsRoot);
+        }
+
+        private void Start()
+        {
+            settingsRoot.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown(toggleSettingsButton))
+                settingsRoot.gameObject.SetActive(!settingsRoot.activeSelf);
+        }
+    }
+}
